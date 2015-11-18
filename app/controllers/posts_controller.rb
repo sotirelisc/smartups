7240bf4fcd2b4f762c2423977bea9136e4879ab9
Zacharias Mitzelos <accounts@mitzelos.com>
2015-11-18 21:29:19 +0000

    Update unsaved controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Successfully posted \"#{@post.title}\"!"
+      redirect_to root_url
+    else
+      @feed_items = current_user.feed.paginate(page: params[:page])
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post has been deleted!"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content, :looking_for)
+    end
+    
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
+    
 end
