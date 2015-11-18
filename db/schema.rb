e0ec616d330260d7b2476db494b4076858f6227e
Zacharias Mitzelos <accounts@mitzelos.com>
2015-11-18 21:18:07 +0000

    Create notification model with references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118204540) do
+ActiveRecord::Schema.define(version: 20151118211407) do
 
   create_table "newsletters", force: :cascade do |t|
     t.string   "first_name"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20151118204540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "notifications", ["post_id"], name: "index_notifications_on_post_id"
+  add_index "notifications", ["user_id"], name: "index_notifications_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
