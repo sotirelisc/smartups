095f0987e624a8810dc1dc8ccb51fc0d8643ebb7
sotirelisc <gtakriz@hotmail.com>
2015-11-19 11:00:37 +0000

    Fix Newsletter layout.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118200917) do
-
-  create_table "newsletters", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20151118211407) do
 
   create_table "newsletters", force: :cascade do |t|
     t.string   "first_name"
