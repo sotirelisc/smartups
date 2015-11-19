1bf6780cff72e87462dd0c42ba1126c025702dbe
Zacharias Mitzelos <accounts@mitzelos.com>
2015-11-19 00:23:53 +0000

    Fix conflicts with master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118211407) do
+ActiveRecord::Schema.define(version: 20151118194820) do
 
   create_table "newsletters", force: :cascade do |t|
     t.string   "first_name"
