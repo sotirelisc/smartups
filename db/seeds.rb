6ab4e314e4e277507bc3834aebdd44fd923b9a3f
sotirelisc <gtakriz@hotmail.com>
2015-11-24 21:51:30 +0000

    Fix relations & change seeds.:scream:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.delete_all
 user = User.new(:first_name => "Admin", :last_name => "Administrator", :email => "admin@smartups.io", :password => "123456789")
 # user.skip_confirmation!
 user.save!
 
-post1 = user.posts.build(:title => "Test title 1", :content => "Test content 1", :looking_for => "Developers")
-post1.save!
-post2 = user.posts.build(:title => "Test title 2", :content => "Test content 2", :looking_for => "Designers")
-post2.save!
+post1 = User.first.posts.new(:user_id => User.first.id, :title => "Test title 1", :content => "Test content 1", :looking_for => "Developers").save!
+post2 = User.first.posts.new(:user_id => User.first.id, :title => "Test title 2", :content => "Test content 2", :looking_for => "Designers").save!
