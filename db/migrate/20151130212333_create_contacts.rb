66d3fed731aee221115c0027e9afeda4e10308d8
Zacharias Mitzelos <accounts@mitzelos.com>
2015-11-30 21:26:58 +0000

    Migrate model contact

--- db/migrate/20151130212333_create_contacts.rb
+++ db/migrate/20151130212333_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :full_name
-      t.integer :phone_number
+      t.integer :phone_number, null: true
       t.string :email
       t.text :message
 
