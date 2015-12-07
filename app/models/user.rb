40357a4ee5debf86dffef4e3e320aebea26f6d2d
sotirelisc <gtakriz@hotmail.com>
2015-12-07 16:34:00 +0000

    Yes, it fixes #11.:punch::punch::punch:

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, presence: true, length: { minimum: 4, maximum: 64 }
+  validates :last_name, presence: true, length: { minimum: 4, maximum: 64 }
 end
