042795b3e63cbd195e3e9391c863c8808cc83c7a
sotirelisc <gtakriz@hotmail.com>
2016-01-16 21:04:46 +0000

    Add interested functionality.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,9 @@ class DashboardController < ApplicationController
   
   def index
   end
+  
+  def interested
+    post = params[:post_id]
+    current_user.interests.create!(post_id: post)
+  end
 end
