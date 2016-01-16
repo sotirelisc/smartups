ce73200f8501ca3d95e284039faa9575efd76566
sotirelisc <gtakriz@hotmail.com>
2016-01-16 21:25:23 +0000

    Create method to get user's notifications.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,4 +12,13 @@ class DashboardController < ApplicationController
     # Create a notification for the post creator.
     p.user.notifications.create!(text: "is interested in", post_id: post)
   end
+  
+  private
+  
+    def get_interested_notifications
+      @notifications = []
+      current_user.notifications.each do |n|
+        @notifications << "#{n.user_id.full_name} #{n.text} your #{n.post_id.title} post."
+      end
+    end
 end
