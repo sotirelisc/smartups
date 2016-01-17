f1551c4543f10b70cb7fba1b22d96720e90c16f0
sotirelisc <gtakriz@hotmail.com>
2016-01-17 19:55:48 +0000

    Fix newsletter links & new BS dropdown.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
   get 'dashboard/profile'
+  get 'dashboard' => 'dashboard#index'
+  
+  get 'newsletters' => 'newsletters#new'
 
   root 'pages#index'
   
