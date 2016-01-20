1a2e04be66d2610564a9b4e88db51904732537ef
sotirelisc <gtakriz@hotmail.com>
2016-01-20 23:14:03 +0000

    Minor frontend fixes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'dashboard/support'
   get 'dashboard/notifications'
   get 'dashboard/news'
+  get 'dashboard/evaluation'
   
   get 'dashboard' => 'dashboard#index'
   get 'profile' => 'dashboard#profile'
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
   get 'notifications' => 'dashboard#notifications'
   get 'search' => 'dashboard#search'
   get 'news' => 'dashboard#news'
+  get 'dashboard/evaluation' => 'dashboard#evaluation'
   get 'sorry' => 'dashboard#support'
   
   get 'newsletters' => 'newsletters#new'
